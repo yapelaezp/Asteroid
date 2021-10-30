d45482e4d93d1035a9bd775414ec1ede1bcf1407
Yesid Alejandro Pelaez <yapelaezp@unal.edu.co>
2021-10-30 00:48:48 -0500

    funcionalidad boton informción

---
+++ app/src/main/java/com/example/asteroid/Informacion.java
@@ -0,0 +1,15 @@
+package com.example.asteroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Informacion extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+
+
+}
