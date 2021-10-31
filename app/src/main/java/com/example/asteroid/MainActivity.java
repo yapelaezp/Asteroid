af2c2b83b0928cfdae9cd4da3e2705e03b709025
Yesid Alejandro Pelaez <yapelaezp@unal.edu.co>
2021-10-30 22:21:10 -0500

    Se agregar Toolbar

--- app/src/main/java/com/example/asteroid/MainActivity.java
+++ app/src/main/java/com/example/asteroid/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.asteroid;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar); 
     }
 
     public void verInformacion (View vistaInformacion){
